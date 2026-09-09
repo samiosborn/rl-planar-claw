8f2484d35c698d44a4cf5cee9d1bee03d1c0da05
Sami Osborn <samiosborn@live.co.uk>
2026-09-09 15:39:27 +0100

    Initial commit

---
+++ src/cube.py
@@ -0,0 +1,49 @@
+# src/cube.py
+
+import pybullet as p
+
+
+class Cube:
+    def __init__(self, body_id: int):
+        self.body_id = body_id
+
+
+    # Yaw angle
+    def get_yaw(self) -> float:
+        _, orientation = p.getBasePositionAndOrientation(self.body_id)
+        _, _, yaw = p.getEulerFromQuaternion(orientation)
+        return yaw
+
+
+    # Get state
+    def get_state(self):
+        position, orientation = p.getBasePositionAndOrientation(self.body_id)
+        linear_velocity, angular_velocity = p.getBaseVelocity(self.body_id)
+
+        _, _, yaw = p.getEulerFromQuaternion(orientation)
+
+        return position, yaw, linear_velocity, angular_velocity
+
+
+    # Reset to a specified position and yaw angle
+    def reset(
+        self,
+        position: tuple[float, float, float],
+        yaw: float,
+    ) -> None:
+
+        # Orientation (quaternion)
+        orientation = p.getQuaternionFromEuler((0.0, 0.0, yaw))
+
+        # Reset position and orientation
+        p.resetBasePositionAndOrientation(
+            self.body_id,
+            basePosition=position,
+            baseOrientation=orientation)
+
+        # Reset linear and angular velocity to zero
+        p.resetBaseVelocity(
+            self.body_id,
+            linearVelocity=(0.0, 0.0, 0.0),
+            angularVelocity=(0.0, 0.0, 0.0))
+    
